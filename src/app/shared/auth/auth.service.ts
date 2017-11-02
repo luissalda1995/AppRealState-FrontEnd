96bda1137b6207666c9bd0983e9a0c401dfbf4e4
Luis Miguel Saldarriaga <luis.salda.1995@gmail.com>
2017-11-01 22:05:19 -0500

    Seguridad

---
+++ src/app/shared/auth/auth.service.ts
@@ -0,0 +1,21 @@
+// src/app/auth/auth.service.ts
+
+import { Injectable } from '@angular/core';
+import decode from 'jwt-decode';
+
+@Injectable()
+export class AuthService {
+
+  public getToken(): string {
+    return localStorage.getItem('token');
+  }
+
+  public isAuthenticated(): boolean {
+    // get the token
+    const token = this.getToken();
+    // return a boolean reflecting 
+    // whether or not the token is expired
+    return true;
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 import { MenuItems } from './menu-items/menu-items';
 import { AccordionAnchorDirective, AccordionLinkDirective, AccordionDirective } from './accordion';
 import { ToggleFullscreenDirective } from './fullscreen/toggle-fullscreen.directive';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './auth/token.interceptor';
 
 @NgModule({
   declarations: [ AccordionAnchorDirective, AccordionLinkDirective, AccordionDirective, ToggleFullscreenDirective ],
   exports:      [ AccordionAnchorDirective, AccordionLinkDirective, AccordionDirective, ToggleFullscreenDirective ],
-  providers: 	[ MenuItems ]
+  providers: 	[ MenuItems, {
+			provide: HTTP_INTERCEPTORS,
+			useClass: TokenInterceptor,
+			multi: true
+    	} 
+	]
 })
 export class SharedModule { }
